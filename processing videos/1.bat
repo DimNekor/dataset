5aae17ff6576f07f2d4f00d7bfea8dab0bfeaf79
prNikson <100094578+prNikson@users.noreply.github.com>
2023-10-26 19:23:55 +0300

    changing bat file. Add choice path to dataset

--- processing videos/1.bat
+++ processing videos/1.bat
@@ -1,10 +1,14 @@
-cd /D D:\Dataset
-mkdir D:\Dataset_new
+@echo off
+set /p "path=Enter path to Dataset:"
+
+cd /D %path%
+set "new_dataset=%CD:~0,2%"
+mkdir %new_dataset%\Dataset_new
 for /r %%a in (*.mp4) do (
 	for %%b in ("%%~dpa\.") do (
 		set "parent=%%~nxb"
 		if %%~zg GTR 464 (
-			ffmpeg -i "%%g" -preset ultrafast "D:\Dataset_new\%parent%\%%~ng.mp4"
+			ffmpeg -i "%%g" -preset ultrafast "%new_dataset%\Dataset_new\%parent%\%%~ng.mp4"
 		)
 	)
 )
