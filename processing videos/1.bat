9a83cee74051cba356172438d58a487076f8e516
prNikson <100094578+prNikson@users.noreply.github.com>
2023-10-26 19:06:31 +0300

    add bat file for convert videos

--- processing videos/1.bat
+++ processing videos/1.bat
@@ -1,7 +1,10 @@
 cd /D D:\Dataset
 mkdir D:\Dataset_new
-for /r %%g in (*.mp4) do (
-	if %%~zg GTR 464 (
-	ffmpeg -i "%%g" -preset ultrafast "D:\Dataset_new\%%~ng.mp4"
+for /r %%a in (*.mp4) do (
+	for %%b in ("%%~dpa\.") do (
+		set "parent=%%~nxb"
+		if %%~zg GTR 464 (
+			ffmpeg -i "%%g" -preset ultrafast "D:\Dataset_new\%parent%\%%~ng.mp4"
+		)
 	)
 )
