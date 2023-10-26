5bdea985d1b3751d22c6008bff0827f017c587ca
prNikson <100094578+prNikson@users.noreply.github.com>
2023-10-26 19:53:38 +0300

    change bat file

--- processing_videos/1.bat
+++ processing_videos/1.bat
@@ -1,14 +1,14 @@
 @echo off
-set /p "path=Enter path to Dataset:"
+set /p "path_=Enter path to Dataset:"
 
-cd /D %path%
+cd /D %path_%
 set "new_dataset=%CD:~0,2%"
 if not exist "%new_dataset%\Dataset_new" mkdir %new_dataset%\Dataset_new
 for /r %%a in (*.mp4) do (
 	for %%b in ("%%~dpa\.") do (
 		set "parent=%%~nxb"
 		if %%~za GTR 464 (
-			if not exist ""%new_dataset%\Dataset_new\%parent%\" mkdir "%new_dataset%\Dataset_new\%parent%\
+			if not exist "%new_dataset%\Dataset_new\%parent%" mkdir %new_dataset%\Dataset_new\%parent%
 			ffmpeg -i "%%a" -preset ultrafast "%new_dataset%\Dataset_new\%parent%\%%~na.mp4"
 		)
 	)
