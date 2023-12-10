6646735785646f2c77e4c988d38d3a9634d5c2d8
prNikson <100094578+prNikson@users.noreply.github.com>
2023-12-10 10:43:43 +0300

    g

--- detect.py
+++ detect.py
@@ -1,5 +1,4 @@
 import cv2
-# import torch
 from ultralytics import YOLO
 from blur_face import blur_face
 import pathlib, glob
@@ -22,33 +21,33 @@ def proccess_videos(videos_url, name_cam):
             ret, frame = video_capture.read()
             if not ret:
                 break
-            results = model.track(source=frame, conf=0.5, classes=0, show=True)
-            for i in results[0]:
-                boxes = i.boxes.numpy()
-                x1, y1, x2, y2 = boxes.xyxy[0] if len(boxes) else [0,0,0,0]
-                x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
-                x1, y1, x2, y2 = max(x1,0), max(y1, 0), max(x2, 0), max(y2, 0)
-                if x1 and x2 and y1 and y2:
-                    region = frame[y1:y2, x1:x2]
-                    blur_photo = blur_face(region)
-                    if flag1 == False and not os.path.isdir("blur-face/{0}".format(name_cam)):
-                        os.mkdir("blur-face/{0}".format(name_cam))
-                        flag1 = True
+            results = model.track(source=frame, conf=0.5, classes=0)
+            track_obj = results[0].boxes.numpy()
+            annotated_frame = results[0].plot()
+            track_id = track_obj.id
+            coord = track_obj.xyxy
+            x1, y1, x2, y2 = coord[0] if len(coord) else [0,0,0,0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            if x1 and x2 and y1 and y2:
+                region = frame[y1:y2, x1:x2]
+                blur_photo = blur_face(region)
+                if flag1 == False and not os.path.isdir("blur-face/{0}".format(name_cam)):
+                    os.mkdir("blur-face/{0}".format(name_cam))
+                    flag1 = True
 
-                    if flag2 == False and not os.path.isdir("detected_from_video_screen/{0}".format(name_cam)):
-                        os.mkdir("detected_from_video_screen/{0}".format(name_cam))
-                        flag2 = True
-                    if not os.path.isdir('detected_from_video_screen/{0}/id{1}'.format(name_cam, count)):
-                        os.mkdir('detected_from_video_screen/{0}/id{1}'.format(name_cam, count))
-                    if not os.path.isdir("blur-face/{0}/id{1}".format(name_cam, count)):
-                        os.mkdir("blur-face/{0}/id{1}".format(name_cam, count))
-                    cv2.imwrite("detected_from_video_screen/{0}/id{1}/photo{2}.jpg".format(name_cam, count, counter), region)
-                    cv2.imwrite("blur-face/{0}/id{1}/yolo_output{2}.jpg".format(name_cam, count, counter), blur_photo)
-                    counter += 1
-            count += 1
-        video_capture.release()
-        cv2.destroyAllWindows()
+                if flag2 == False and not os.path.isdir("detected_from_video_screen/{0}".format(name_cam)):
+                    os.mkdir("detected_from_video_screen/{0}".format(name_cam))
+                    flag2 = True
+                if not os.path.isdir('detected_from_video_screen/{0}/id{1}'.format(name_cam, track_id)):
+                    os.mkdir('detected_from_video_screen/{0}/id{1}'.format(name_cam, track_id))
+                if not os.path.isdir("blur-face/{0}/id{1}".format(name_cam, track_id)):
+                    os.mkdir("blur-face/{0}/id{1}".format(name_cam, track_id))
+                cv2.imwrite("detected_from_video_screen/{0}/id{1}/photo{2}.jpg".format(name_cam, track_id, counter), region)
+                cv2.imwrite("blur-face/{0}/id{1}/yolo_output{2}.jpg".format(name_cam, track_id, counter), blur_photo)
+                counter += 1
+    video_capture.release()
+cv2.destroyAllWindows()
 
 for i in range(len(label_videos)):
-    path = pathlib.Path(label_videos[i]).parts
-    proccess_videos(label_videos[i], path[2])
+    path = pathlib.Path(label_videos[i+2]).parts
+    proccess_videos(label_videos[i+2], path[2])
